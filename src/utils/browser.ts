b957efdf82cf57e90c1b54972d054125d2e696fb
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2024-12-07 21:40:11 +0700

    feat: move from puppeteer to playwright

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,38 +1,48 @@
-import puppeteer, { Browser } from "puppeteer";
+import { Browser, BrowserContext, chromium } from "@playwright/test";
 import { config } from "../config/config";
-import { PuppeteerBlocker } from "@ghostery/adblocker-puppeteer";
+import { fullLists, PlaywrightBlocker } from "@ghostery/adblocker-playwright";
 
 let browserInstance: Browser | null = null;
-let blocker: PuppeteerBlocker | null = null;
+let contextInstance: BrowserContext | null = null;
+let blocker: PlaywrightBlocker | null = null;
 
-export async function initBrowser(): Promise<Browser> {
+export async function initBrowser(): Promise<BrowserContext> {
   if (!browserInstance) {
-    browserInstance = await puppeteer.launch({
+    browserInstance = await chromium.launch({
       headless: true,
       args: config.browserArgs,
-      defaultViewport: {
-        width: 1440,
-        height: 756,
-        deviceScaleFactor: 5 / 3,
-      },
     });
   }
-  return browserInstance;
+
+  if (!contextInstance) {
+    contextInstance = await browserInstance.newContext({
+      viewport: {
+        width: 1440,
+        height: 756,
+      },
+      deviceScaleFactor: 5 / 3,
+      userAgent: config.userAgent,
+    });
+  }
+
+  return contextInstance;
 }
 
-export async function getBlocker(): Promise<PuppeteerBlocker> {
+export async function getBlocker(): Promise<PlaywrightBlocker> {
   if (!blocker) {
-    blocker = await PuppeteerBlocker.fromLists(fetch, [
-      "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
-      "https://easylist.to/easylist/easylist.txt",
-      "https://easylist.to/easylist/fanboy-social.txt",
-      "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
-    ]);
+    blocker = await PlaywrightBlocker.fromLists(fetch, fullLists, {
+      enableCompression: true,
+    });
   }
   return blocker;
 }
 
 export async function closeBrowser(): Promise<void> {
+  if (contextInstance) {
+    await contextInstance.close();
+    contextInstance = null;
+  }
+
   if (browserInstance) {
     await browserInstance.close();
     browserInstance = null;
